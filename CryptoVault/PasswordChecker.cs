d3a15e64aa87999572dbec19e76da03649c7e3a0
gambergeurr <pepsimaann@proton.me>
2026-04-21 15:41:47 +0200

    password anlyzer module

---
+++ CryptoVault/PasswordChecker.cs
@@ -0,0 +1,59 @@
+﻿
+
+namespace CryptoVault;
+
+public static class PasswordChecker
+{
+    public static bool CheckCaps(string password)
+    {
+        foreach (char c in password)
+        {
+            if (char.IsUpper(c))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static bool CheckLower(string password)
+    {
+        foreach (char c in password)
+        {
+            if (!char.IsUpper(c))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static bool CheckLenght(string password)
+    {
+        return password.Length >= 10;
+    }
+
+    public static bool CheckNumber(string password)
+    {
+        foreach (char c in password)
+        {
+            if (char.IsDigit(c))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static bool CheckSpecial(string password)
+    {
+        foreach (char c in password)
+        {
+            if (!char.IsLetter(c) && !char.IsNumber(c))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
