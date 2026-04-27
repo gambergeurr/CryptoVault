9ed0de9c5f424e929864b44738f87a4b00a99c84
gambergeurr <pepsimaann@proton.me>
2026-04-27 16:12:43 +0200

    steganoServices.cs: implement payload size insertion

--- CryptoVault/utils/steganoServices.cs
+++ CryptoVault/utils/steganoServices.cs
@@ -28,6 +28,33 @@ public static class steganoServices
         Random r = new Random(seed);
         r.Shuffle(order);
 
+        uint payloadSize = (uint)data.Length * 8;
+
+        // hide payload size in 8 first bytes for later extraction
+        for (int i = 0; i < 8; i++)
+        {
+            //x y pixel coordinate
+            int x = order[i] % camoImage.Width;
+            int y = order[i] / camoImage.Width;
+            
+            Rgba32 pixel = camoImage[x, y];
+
+            // index of the next nibble to hide
+            int index = 31 - (4 * i);
+            
+            // shifting the payloadSize to get the index bit to postition 0 and setting every other bit to 0
+            uint bitR = (payloadSize >> index) & 1;
+            uint bitG = (payloadSize >> (index - 1)) & 1;
+            uint bitB = (payloadSize >> (index - 2)) & 1;
+            uint bitA = (payloadSize >> (index - 3)) & 1;
+            
+            // setting each pixel's lsb to 0 and using OR operator to set it to desired value
+            pixel.R = (byte)((pixel.R & 254) | bitR);
+            pixel.G = (byte)((pixel.G & 254) | bitG);
+            pixel.B = (byte)((pixel.B & 254) | bitB);
+            pixel.A = (byte)((pixel.A & 254) | bitA);
+        }
+
         return null;
     }
 }
