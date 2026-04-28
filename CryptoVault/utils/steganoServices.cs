f0f78c6b7a0f52c146cd9fdac030a9b17576689a
gambergeurr <pepsimaann@proton.me>
2026-04-28 09:51:37 +0200

    steganoServices.cs add payload hide logic

--- CryptoVault/utils/steganoServices.cs
+++ CryptoVault/utils/steganoServices.cs
@@ -9,7 +9,7 @@ namespace CryptoVault;
 
 public static class steganoServices
 {
-    public static Image Hide(byte[] data, string password, Image<Rgba32> camoImage)
+    private static int[] CreateOrderArray(string password, Image<Rgba32> camoImage)
     {
         // convert password into an int seed
         byte[] passwordHash = SHA256.HashData(Encoding.UTF8.GetBytes(password));
@@ -27,10 +27,16 @@ public static class steganoServices
         // instance random class from the password derived seed + order shuffle
         Random r = new Random(seed);
         r.Shuffle(order);
+        
+        return order;
+    }
+    public static Image Hide(byte[] data, string password, Image<Rgba32> camoImage)
+    {
+        int[] order = CreateOrderArray(password, camoImage);
 
         uint payloadSize = (uint)data.Length * 8;
 
-        // hide payload size in 8 first bytes for later extraction
+        // hide payload size in 8 first pixels (8 * 4 = 32bit) for later etraction
         for (int i = 0; i < 8; i++)
         {
             //x y pixel coordinate
@@ -57,28 +63,40 @@ public static class steganoServices
             camoImage[x, y] = pixel;
         }
 
+        for (int i = 0; i < payloadSize / 4; i++)
+        {
+            //x y pixel coordinate
+            int x = order[i + 8] % camoImage.Width;
+            int y = order[i + 8] / camoImage.Width;
+            
+            Rgba32 pixel = camoImage[x, y];
+            
+            // index of the next nibble in the [i / 2]th byte
+            int index = 7 - ((i * 4) % 8);
+    
+            // 4. getting the indexed byte to position 0 and setting every other to 0
+            uint bitR = (uint)((data[i / 2] >> index) & 1);
+            uint bitG = (uint)((data[i / 2] >> (index - 1)) & 1);
+            uint bitB = (uint)((data[i / 2] >> (index - 2)) & 1);
+            uint bitA = (uint)((data[i / 2] >> (index - 3)) & 1);
+            
+            // setting each pixel's lsb to 0 and using OR operator to set it to desired value
+            pixel.R = (byte)((pixel.R & 254) | bitR);
+            pixel.G = (byte)((pixel.G & 254) | bitG);
+            pixel.B = (byte)((pixel.B & 254) | bitB);
+            pixel.A = (byte)((pixel.A & 254) | bitA);
+            
+            camoImage[x, y] = pixel;
+        }
+
         return camoImage;
     }
 
     public static byte[] Unhide(string password, Image<Rgba32> camoImage)
     {
-        // convert password into an int seed
-        byte[] passwordHash = SHA256.HashData(Encoding.UTF8.GetBytes(password));
-        int seed = BitConverter.ToInt32(passwordHash, 0);
-        
-        int totalPixels = camoImage.Height * camoImage.Width;
-        int[] order = new int[totalPixels];
+        int[] order = CreateOrderArray(password, camoImage);
 
-        // fill array
-        for (int i = 0; i < totalPixels; i++)
-        {
-            order[i] = i;
-        }
-        
-        // instance random class from the password derived seed + order shuffle
-        Random r = new Random(seed);
-        r.Shuffle(order);
-
+        uint payloadSize = 0;
         for (int i = 0; i < 8; i++)
         {
             //x y pixel coordinate
@@ -87,7 +105,11 @@ public static class steganoServices
             
             Rgba32 pixel = camoImage[x, y];
             
-            Console.Write($"{pixel.R & 1}{pixel.G & 1}{pixel.B & 1}{pixel.A & 1} ");
+            // building the integer from msb to lsb
+            payloadSize = (uint)((payloadSize << 1) | pixel.R & 1);
+            payloadSize = (uint)((payloadSize << 1) | pixel.G & 1);
+            payloadSize = (uint)((payloadSize << 1) | pixel.B & 1);
+            payloadSize = (uint)((payloadSize << 1) | pixel.A & 1);
         }
         
         return null;
