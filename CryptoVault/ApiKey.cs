8418eca12bcbf4d96cefa11224fac2cd548a16e9
gambergeurr <pepsimaann@proton.me>
2026-04-30 12:01:27 +0200

    create ApiKey.cs class and ApiManager.axaml userControl

---
+++ CryptoVault/ApiKey.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Avalonia.Platform.Storage;
+
+namespace CryptoVault;
+
+public class ApiKey
+{
+    public string Name { get; set; }
+    public string Key { get; set; }
+    public string Desctiption { get; set; }
+    public DateOnly ExpirationDate { get; set; }
+}
---
+++ CryptoVault/modules/ApiManager.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace CryptoVault.modules;
+
+public partial class ApiManager : UserControl
+{
+    public ApiManager()
+    {
+        InitializeComponent();
+    }
+}
